a306f6220f85ef9fe256cd8628c040667afb2400
Estelle Olawinski <estelle.olawinski@me.com>
2019-01-22 12:12:35 +0100

    still doing it

--- js/script.js
+++ js/script.js
@@ -1,40 +1,75 @@
-function selectNumber() {
-  var classes = ["one", "two", "three", "four", "five", "six", "seven"];
-  var randomIndexLeft = Math.floor(Math.random() * 7);
-  var randomIndexMiddle = Math.floor(Math.random() * 7);
-  var randomIndexRight = Math.floor(Math.random() * 7);
-  var chosenClassLeft = classes[randomIndexLeft];
-  var chosenClassMiddle = classes[randomIndexMiddle];
-  var chosenClassRight = classes[randomIndexRight];
-  $(".left").removeClass("one two three four five six seven");
-  $(".left").addClass(chosenClassLeft);
-  $(".middle").removeClass("one two three four five six seven");
-  $(".middle").addClass(chosenClassMiddle);
-  $(".right").removeClass("one two three four five six seven");
-  $(".right").addClass(chosenClassRight);
+// function selectNumber() {
+//   var classes = ["one", "two", "three", "four", "five", "six", "seven"];
+//   var randomIndexLeft = Math.floor(Math.random() * 7);
+//   var randomIndexMiddle = Math.floor(Math.random() * 7);
+//   var randomIndexRight = Math.floor(Math.random() * 7);
+//   var chosenClassLeft = classes[randomIndexLeft];
+//   var chosenClassMiddle = classes[randomIndexMiddle];
+//   var chosenClassRight = classes[randomIndexRight];
+//   $(".left").removeClass("one two three four five six seven");
+//   $(".left").addClass(chosenClassLeft);
+//   $(".middle").removeClass("one two three four five six seven");
+//   $(".middle").addClass(chosenClassMiddle);
+//   $(".right").removeClass("one two three four five six seven");
+//   $(".right").addClass(chosenClassRight);
+// }
+
+// selectNumber();
+// $(".click-btn").click(function() {
+//   selectNumber();
+// });
+
+// -----------------------------------------------------------------------
+
+var selectNumber = [
+  { category: "number", src: "./images/image-one.jpg", value: "one" },
+  { category: "number", src: "./images/image-two.jpg", value: "two" },
+  { category: "number", src: "./images/image-three.jpg", value: "three" },
+  { category: "number", src: "./images/image-four.jpg", value: "four" },
+  { category: "number", src: "./images/image-five.jpg", value: "five" },
+  { category: "number", src: "./images/image-six.jpg", value: "six" },
+  { category: "number", src: "./images/image-seven.jpg", value: "seven" }
+];
+
+function updateImage(array) {
+  numero1 = Math.floor(Math.random() * array.length);
+  numero2 = Math.floor(Math.random() * array.length);
+  numero3 = Math.floor(Math.random() * array.length);
+  $(".left").attr("src", array[numero1].src);
+  $(".middle").attr("src", array[numero2].src);
+  $(".right").attr("src", array[numero3].src);
+
+  if (numero1 === numero2 && numero2 === numero3) {
+    console.log("You Win!");
+  } else {
+    console.log("You big loser!");
+  }
 }
 
-selectNumber();
+// ------------------------------------------------------------
+
+updateImage(selectNumber);
+
+// ------------------------------------------------------------
+
 $(".click-btn").click(function() {
-  selectNumber();
+  updateImage(selectNumber);
 });
 
-function lineOne() {
-  var leftOne = $(".left").hasClass("one");
-  var middleOne = $(".middle").hasClass("one");
-  var rightOne = $(".right").hasClass("one");
+// -----------------------------------------------------------------------
 
-  if (isRock) {
-    playerRock();
-    //opponent: rock ties, paper we lose, scissors we win
-  } else if (isPaper) {
-    playerPaper();
-    //opponent: paper ties, scissors we lose, rock we win
-  } else {
-    playerScissors();
-    //opponent: scissors ties, rock we lose, paper we win
-  }
+// function equalNumbers() {
+//   var left = $(".left").hasClass("one");
+//   var middle = $(".middle").hasClass("one");
+//   var right = $(".right").hasClass("one");
 
-  // show the game over screen
-  $(".game-end").addClass("showing");
-}
+//   if ((left === middle) === right) {
+//   } else if (isPaper) {
+//   } else {
+//   }
+
+//   // show the game over screen
+//   $(".game-end").addClass("showing");
+// }
+
+// console.log(numberOne(selectNumber));
