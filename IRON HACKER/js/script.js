67b9ffbb8a11dcb7644936b36af084821d7d6784
Estelle Olawinski <estelle.olawinski@me.com>
2019-01-24 16:39:37 +0100

    finshed

---
+++ IRON HACKER/js/script.js
@@ -0,0 +1,135 @@
+// function selectNumber() {
+//   var classes = ["one", "two", "three", "four", "five", "six", "seven"];
+//   var randomIndexLeft = Math.floor(Math.random() * 7);
+//   var randomIndexMiddle = Math.floor(Math.random() * 7);
+//   var randomIndexRight = Math.floor(Math.random() * 7);
+//   var chosenClassLeft = classes[randomIndexLeft];
+//   var chosenClassMiddle = classes[randomIndexMiddle];
+//   var chosenClassRight = classes[randomIndexRight];
+//   $(".left").removeClass("one two three four five six seven");
+//   $(".left").addClass(chosenClassLeft);
+//   $(".middle").removeClass("one two three four five six seven");
+//   $(".middle").addClass(chosenClassMiddle);
+//   $(".right").removeClass("one two three four five six seven");
+//   $(".right").addClass(chosenClassRight);
+// }
+
+// selectNumber();
+// $(".click-btn").click(function() {
+//   selectNumber();
+// });
+
+var bet = 2;
+var credits = 300;
+var gain = 40;
+var extraGain = 50;
+var jackpot = ["012", "021", "120", "102", "201", "210"];
+var isJackpot = "Hide & Seek";
+var geniusPrice = ["1811", "1118", "8111", "1181", "1118"];
+var isGenius = "GeniusOfEvil";
+
+// -----------------------------------------------------------------------
+
+var selectNumber = [
+  { extras: false, src: "./images/image-three.jpg", value: "one" },
+  { extras: false, src: "./images/image-five.jpg", value: "two" },
+  { extras: true, src: "./images/image-ten.jpg", value: "ten" },
+  { extras: true, src: "./images/image-seven.jpg", value: "three" },
+  { extras: false, src: "./images/image-four.jpg", value: "four" },
+  { extras: true, src: "./images/image-nine.jpg", value: "nine" },
+  { extras: true, src: "./images/image-one.jpg", value: "five" },
+  { extras: false, src: "./images/image-six.jpg", value: "six" },
+  { extras: true, src: "./images/image-eleven.jpg", value: "eleven" },
+  { extras: true, src: "./images/image-two.jpg", value: "seven" },
+  { extras: true, src: "./images/image-eight.jpg", value: "eight" }
+];
+
+function updateImage(array) {
+  numero1 = Math.floor(Math.random() * array.length);
+  numero2 = Math.floor(Math.random() * array.length);
+  numero3 = Math.floor(Math.random() * array.length);
+  $(".left").attr("src", array[numero1].src);
+  $(".middle").attr("src", array[numero2].src);
+  $(".right").attr("src", array[numero3].src);
+
+  isJackpot = numero1.toString() + numero2.toString() + numero3.toString();
+
+  var stillPoints = document.querySelector(".totalPoints strong");
+  if (numero1 === numero2 && numero2 === numero3) {
+    credits += gain;
+    console.log("Just luck! " + credits);
+    alert("You're Lucky!");
+  }
+  if (jackpot.indexOf(isJackpot) !== -1) {
+    credits += extraGain;
+    console.log("JACKPOT MY FRIIIEND" + credits);
+    alert("SPECIAL BEST TEAM COMBINATION");
+  }
+  if (geniusPrice.indexOf(isGenius) !== -1) {
+    credits += extraGain;
+    console.log("SPECIAL EVIL GENIUS PRICE!" + credits);
+    alert("SPECIAL EVIL GENIUS COMBINATION!");
+  } else {
+    console.log("You loose, loser! " + credits);
+  }
+  stillPoints.innerHTML = credits;
+}
+
+// -----------------------------------------------------------------------
+
+updateImage(selectNumber);
+
+// -----------------------------------------------------------------------
+
+$(".click-btn").click(function() {
+  var nextAmount = credits - bet;
+  if (nextAmount >= 0) {
+    credits = nextAmount;
+    updateImage(selectNumber);
+  }
+  return;
+});
+
+// -----------------------------------------------------------------------
+
+$(".game-end button").click(function() {
+  $(".game-end").removeClass("showing");
+  $(".container").removeClass("left middle right");
+});
+
+// -----------------------------------------------------------------------
+
+$(".click-btn").mouseenter(function() {
+  $(".click-btn").css({ color: "white" });
+  $(".click-btn").mouseleave(function() {
+    $(".click-btn").css({ color: "#5e66ae" });
+  });
+});
+
+$(".click-btn").mouseenter(function() {
+  $(".click-btn").css({ backgroundColor: "#e45668" });
+  $(".click-btn").mouseleave(function() {
+    $(".click-btn").css({ backgroundColor: "white" });
+  });
+});
+
+$(".click-btn").mouseenter(function() {
+  $(".click-btn").css({ borderColor: "white" });
+  $(".click-btn").mouseleave(function() {
+    $(".click-btn").css({ borderColor: "#e45668" });
+  });
+});
+
+// -----------------------------------------------------------------------
+
+$(".left").mouseenter(function() {
+  var monImage = $(this).find("img");
+  $(monImage).animate({ opacity: "0.16" });
+});
+
+$(".left").mouseleave(function() {
+  var monImage = $(this).find("img");
+  $(monImage)
+    .stop()
+    .animate({ opacity: "1" });
+});
--- js/script.js
+++ js/script.js
@@ -1,67 +1,29 @@
-// function selectNumber() {
-//   var classes = ["one", "two", "three", "four", "five", "six", "seven"];
-//   var randomIndexLeft = Math.floor(Math.random() * 7);
-//   var randomIndexMiddle = Math.floor(Math.random() * 7);
-//   var randomIndexRight = Math.floor(Math.random() * 7);
-//   var chosenClassLeft = classes[randomIndexLeft];
-//   var chosenClassMiddle = classes[randomIndexMiddle];
-//   var chosenClassRight = classes[randomIndexRight];
-//   $(".left").removeClass("one two three four five six seven");
-//   $(".left").addClass(chosenClassLeft);
-//   $(".middle").removeClass("one two three four five six seven");
-//   $(".middle").addClass(chosenClassMiddle);
-//   $(".right").removeClass("one two three four five six seven");
-//   $(".right").addClass(chosenClassRight);
-// }
-
-// selectNumber();
-// $(".click-btn").click(function() {
-//   selectNumber();
-// });
-
-var bet = 2;
-var credits = 200;
-var gain = 10;
-
 // -----------------------------------------------------------------------
 
-var selectNumber = [
-  { category: "number", src: "./images/image-one.jpg", value: "one" },
-  { category: "number", src: "./images/image-two.jpg", value: "two" },
-  { category: "number", src: "./images/image-three.jpg", value: "three" },
-  { category: "number", src: "./images/image-four.jpg", value: "four" },
-  { category: "number", src: "./images/image-five.jpg", value: "five" },
-  { category: "number", src: "./images/image-six.jpg", value: "six" },
-  { category: "number", src: "./images/image-seven.jpg", value: "seven" }
-];
-
-function updateImage(array) {
-  numero1 = Math.floor(Math.random() * array.length);
-  numero2 = Math.floor(Math.random() * array.length);
-  numero3 = Math.floor(Math.random() * array.length);
-  $(".left").attr("src", array[numero1].src);
-  $(".middle").attr("src", array[numero2].src);
-  $(".right").attr("src", array[numero3].src);
-
-  var jeanmichel = document.querySelector(".totalPoints strong");
-  if (numero1 === numero2 && numero2 === numero3) {
-    credits += gain;
-    console.log("You Win!" + credits);
-  } else {
-    console.log("You big loser!" + credits);
-  }
-  jeanmichel.innerHTML = credits;
-}
-
-// ------------------------------------------------------------
-
-updateImage(selectNumber);
-
-// ------------------------------------------------------------
-
-$(".click-btn").click(function() {
-  credits -= bet;
-  updateImage(selectNumber);
+$(".right").mouseenter(function() {
+  $(".colour").css({ opacity: "0", display: "block" });
+  $(".colour")
+    .stop()
+    .animate({ opacity: "1" }, 300);
 });
 
-// -----------------------------------------------------------------------
+$(".right").mouseleave(function() {
+  $(".colour").css({ opacity: "0", display: "block" });
+  $(".colour")
+    .stop()
+    .animate({ opacity: "0" }, 300);
+});
+
+$(".left").mouseenter(function() {
+  $(".colorfull").css({ opacity: "0", display: "block" });
+  $(".colorfull")
+    .stop()
+    .animate({ opacity: "1" }, 300);
+});
+
+$(".left").mouseleave(function() {
+  $(".colorfull").css({ opacity: "0", display: "block" });
+  $(".colorfull")
+    .stop()
+    .animate({ opacity: "0" }, 300);
+});
